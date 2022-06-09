41e4d99158da238ec298111e096a38baf4344bad
IsaPirola <belapirola@gmail.com>
2022-06-08 22:03:49 -0300

    feat: create database with table of pets

--- api/app/Http/Controllers/PetController.php
+++ api/app/Http/Controllers/PetController.php
@@ -2,12 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pet;
 use Illuminate\Http\Request;
 
 class PetController extends Controller
 {
+    /**
+     * Returns the list of registered pets
+     *
+     * @return Collection
+     */
     public function index()
     {
-        echo "estou na lista de pets";
+        return Pet::get();
     }
 }
